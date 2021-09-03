3332199cfde01d2ddb7e07a61bcb406be76b37f4
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-09-03 23:43:52 +0500

    Added frontDiv to homepage

--- src/PageModules/HomeModule.js
+++ src/PageModules/HomeModule.js
@@ -11,22 +11,34 @@ const HomeModule = (() => {
         },
         createElements: function() {
             this.homeContent = document.createElement('div');
+            this.frontDiv = document.createElement('div');
+            this.welcomeDiv = document.createElement('div');
+            this.welcomeDivText1 = document.createElement('p');
+            this.welcomeDivText2 = document.createElement('p');
             this.mottoDiv = document.createElement('div');
             this.mottoText = document.createElement('p');
         },
         setId: function() {
             this.homeContent.id = 'homeContent';
+            this.frontDiv.id = 'frontDiv';
+            this.welcomeDiv.id = 'welcomeDiv';
+            this.welcomeDivText1.id = 'welcomeDivText1';
+            this.welcomeDivText2.id = 'welcomeDivText2';
             this.mottoDiv.id = 'mottoDiv';
             this.mottoText.id = 'mottoText';
         },
         setContent: function() {
-            this.mottoText = 'The most succulent experience in town.';
+            this.welcomeDivText1.textContent = 'Welcome to';
+            this.welcomeDivText2.textContent = 'Savory';
+            this.mottoText.textContent = 'The most succulent experience in town.';
         },
     };
     function appendContent() {
         homeObject.init();
+        this.welcomeDiv.append(this.welcomeDivText1, this.welcomeDivText2);
         this.mottoDiv.append(this.mottoText);
-        this.homeContent.append(this.mottoDiv);
+        this.frontDiv.append(this.welcomeDiv, this.mottoDiv);
+        this.homeContent.append(this.frontDiv);
         this.pageContent.append(this.homeContent);
     }
     const attach = appendContent.bind(homeObject);
