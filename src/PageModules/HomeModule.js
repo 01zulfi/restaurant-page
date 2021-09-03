0d2e3ba198e3f410d655bafab3fd4ab5e47bfd84
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-09-04 00:35:09 +0500

    Added chf.png; added messageDiv in homepage

--- src/PageModules/HomeModule.js
+++ src/PageModules/HomeModule.js
@@ -17,6 +17,9 @@ const HomeModule = (() => {
             this.welcomeDivText2 = document.createElement('p');
             this.mottoDiv = document.createElement('div');
             this.mottoText = document.createElement('p');
+            this.messageDiv = document.createElement('div');
+            this.messageDivText1 = document.createElement('div');
+            this.messageDivText2 = document.createElement('div');
         },
         setId: function() {
             this.homeContent.id = 'homeContent';
@@ -26,19 +29,27 @@ const HomeModule = (() => {
             this.welcomeDivText2.id = 'welcomeDivText2';
             this.mottoDiv.id = 'mottoDiv';
             this.mottoText.id = 'mottoText';
+            this.messageDiv.id = 'messageDiv';
+            this.messageDivText1.id = 'messageDivText1';
+            this.messageDivText2.id = 'messageDivText2'
         },
         setContent: function() {
             this.welcomeDivText1.textContent = 'Welcome to';
             this.welcomeDivText2.textContent = 'Savory';
             this.mottoText.textContent = 'The most succulent experience in town.';
-        },
+            this.messageDivText1.textContent = 'For the love of cooking.';
+            this.messageDivText2.textContent = `Cooking is art and food is our passion.
+                                                From choosing the best ingredients to the
+                                                presentation, everything is handled with utmost care.`
+        }, 
     };
     function appendContent() {
         homeObject.init();
         this.welcomeDiv.append(this.welcomeDivText1, this.welcomeDivText2);
         this.mottoDiv.append(this.mottoText);
         this.frontDiv.append(this.welcomeDiv, this.mottoDiv);
-        this.homeContent.append(this.frontDiv);
+        this.messageDiv.append(this.messageDivText1, this.messageDivText2);
+        this.homeContent.append(this.frontDiv, this.messageDiv);
         this.pageContent.append(this.homeContent);
     }
     const attach = appendContent.bind(homeObject);
